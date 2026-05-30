d6ba1985d75220db5e3e62937c6fbdde8a9f4865
paituo <330435863@qq.com>
2026-05-30 12:05:07 +0800

    openclaw/app: add YAML struct fields for compaction, summary, planner
    Add missing yaml tags to agentRunConfig for:
    - session_summary_injection_mode, sync_summary_intra_run
    - context_compaction_threshold_ratio, context_compaction_tool_result_max_tokens,
      context_compaction_keep_recent_requests
    - enable_detailed_context_metrics
    - context_compaction_force_clean_tool_names, context_compaction_keep_tool_names
    - planner_type, planner_config
    
    Required because loadConfigFile uses KnownFields(true).
    
    Reimplements-PAITUO: feat(run_options): add YAML config struct fields
    Upstream-Compatible: yes

--- openclaw/app/run_options.go
+++ openclaw/app/run_options.go
@@ -1145,7 +1145,12 @@ type agentRunConfig struct {
 	Type *string `yaml:"type,omitempty"`
 
 	AddSessionSummary                             *bool `yaml:"add_session_summary,omitempty"`
+	SessionSummaryInjectionMode                   *string  `yaml:"session_summary_injection_mode,omitempty"`
+	SyncSummaryIntraRun                           *bool    `yaml:"sync_summary_intra_run,omitempty"`
 	EnableContextCompaction                       *bool `yaml:"enable_context_compaction,omitempty"`
+	ContextCompactionThresholdRatio               *float64 `yaml:"context_compaction_threshold_ratio,omitempty"`
+	ContextCompactionToolResultMaxTokens          *int     `yaml:"context_compaction_tool_result_max_tokens,omitempty"`
+	ContextCompactionKeepRecentRequests           *int     `yaml:"context_compaction_keep_recent_requests,omitempty"`
 	ContextCompactionOversizedToolResultMaxTokens *int  `yaml:"context_compaction_oversized_tool_result_max_tokens,omitempty"`
 	MaxHistoryRuns                                *int  `yaml:"max_history_runs,omitempty"`
 	MaxLLMCalls                                   *int  `yaml:"max_llm_calls,omitempty"`
@@ -1153,6 +1158,13 @@ type agentRunConfig struct {
 	PreloadMemory                                 *int  `yaml:"preload_memory,omitempty"`
 	DisablePostToolPrompt                         *bool `yaml:"disable_post_tool_prompt,omitempty"`
 	DisablePostToolPromptCamel                    *bool `yaml:"disablePostToolPrompt,omitempty"`
+
+	EnableDetailedContextMetrics             *bool    `yaml:"enable_detailed_context_metrics,omitempty"`
+	ContextCompactionForceCleanToolNames     []string `yaml:"context_compaction_force_clean_tool_names,omitempty"`
+	ContextCompactionKeepToolNames           []string `yaml:"context_compaction_keep_tool_names,omitempty"`
+
+	PlannerType   string         `yaml:"planner_type"`
+	PlannerConfig map[string]any `yaml:"planner_config"`
 
 	Instruction      *string  `yaml:"instruction,omitempty"`
 	InstructionFiles []string `yaml:"instruction_files,omitempty"`
